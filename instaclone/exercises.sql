fdd2880eb87cd44afe92b9dc315b141ed6d8edb9
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2024-01-16 23:05:04 +0300

    indexing & benchmarking

--- instaclone/exercises.sql
+++ instaclone/exercises.sql
@@ -18,3 +18,21 @@ SELECT username, COUNT(*)
 FROM users
 JOIN likes ON likes.user_id = users.id
 GROUP BY username;
+
+-- Creating index example
+CREATE INDEX ON users (username);
+-- we can manually set name of index
+CREATE INDEX users_username_idx ON users (username);
+
+-- Deleting index:
+DROP INDEX users_username_idx;
+
+-- analyzing benchmark
+EXPLAIN ANALYZE SELECT *
+FROM users
+WHERE username = 'Emil30'
+
+-- To check indexes inside the database:
+SELECT relname, relkind
+FROM pg_class
+WHERE relkind = 'i'
